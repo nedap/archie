81d38d380a94c2342c01bc13939dc90c40060bba
Pieter Bos <pieter.bos@nedap.com>
2015-11-03 14:18:36 +0100

    Fix terminology code parsing. Strange thing in archetype metadata: lifecycle is a terminology code, but what is the terminology id?

--- archetypemodel/src/main/java/com/nedap/archie/base/terminology/TerminologyCode.java
+++ archetypemodel/src/main/java/com/nedap/archie/base/terminology/TerminologyCode.java
@@ -1,5 +1,10 @@
 package com.nedap.archie.base.terminology;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by pieter.bos on 15/10/15.
  */
@@ -11,9 +16,24 @@ public class TerminologyCode {
     public TerminologyCode() {
     }
 
-    //for json-mapping
-    public TerminologyCode(String terminologyId) {//todo
-        this.terminologyId = terminologyId;
+    @JsonCreator
+    public static TerminologyCode createFromString(String terminologyString) {
+        if(terminologyString.isEmpty()) {
+            return null;
+        }
+        //'[' NAME_CHAR+ ( '(' NAME_CHAR+ ')' )? '::' NAME_CHAR+ ']' ;
+        Pattern pattern = Pattern.compile("\\[(?<terminologyId>.+)(\\((?<terminologyVersion>.+)\\))?::(?<codeString>.+)\\]");
+        Matcher matcher = pattern.matcher(terminologyString);
+        TerminologyCode result = new TerminologyCode();
+        if(matcher.matches()) {
+            result.terminologyId = matcher.group("terminologyId");
+            result.codeString = matcher.group("codeString");
+            result.terminologyVersion = matcher.group("terminologyVersion");
+
+        } else {
+            result.codeString = terminologyString;
+        }
+        return result;
     }
 
     public String getTerminologyId() {
