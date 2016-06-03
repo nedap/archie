0705b640ce670ca901988b00f83323c38b15e1d9
Pieter Bos <pieter.bos@nedap.com>
2016-06-03 20:14:01 +0200

    Add link fields

--- src/main/java/com/nedap/archie/rm/archetypes/Link.java
+++ src/main/java/com/nedap/archie/rm/archetypes/Link.java
@@ -1,7 +1,41 @@
 package com.nedap.archie.rm.archetypes;
 
+import com.nedap.archie.rm.datavalues.DvEHRURI;
+import com.nedap.archie.rm.datavalues.DvText;
+
+import javax.xml.bind.annotation.XmlType;
+
 /**
  * Created by pieter.bos on 04/11/15.
  */
+@XmlType(name = "LINK")
 public class Link {
+
+    private DvText meaning;
+    private DvText type;
+    private DvEHRURI target;
+
+    public DvText getMeaning() {
+        return meaning;
+    }
+
+    public void setMeaning(DvText meaning) {
+        this.meaning = meaning;
+    }
+
+    public DvText getType() {
+        return type;
+    }
+
+    public void setType(DvText type) {
+        this.type = type;
+    }
+
+    public DvEHRURI getTarget() {
+        return target;
+    }
+
+    public void setTarget(DvEHRURI target) {
+        this.target = target;
+    }
 }
