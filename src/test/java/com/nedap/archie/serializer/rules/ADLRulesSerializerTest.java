c976dcd145ad5f8d324aa63021ffd81c66ccd4bd
Pieter Bos <pieter.bos@nedap.com>
2016-06-15 15:37:51 +0200

    Remove printlns from test

--- src/test/java/com/nedap/archie/serializer/rules/ADLRulesSerializerTest.java
+++ src/test/java/com/nedap/archie/serializer/rules/ADLRulesSerializerTest.java
@@ -21,7 +21,7 @@ public class ADLRulesSerializerTest {
         Archetype archetype = load("simplearithmetic.adls");
         String serializedADL = ADLArchetypeSerializer.serialize(archetype);
 
-        //System.out.println(serializedADL);
+        
         assertTrue(serializedADL.contains("$arithmetic_test:Integer ::= 3 * 5 + 2 * 2 - 15 + 4;"));
         assertTrue(serializedADL.contains("$boolean_false_test:Boolean ::= 3 > 5 + 6 * 7 + 3 * 23 + 8 /  (1 + 2) ;"));
         assertTrue(serializedADL.contains("$boolean_true_test:Boolean ::= 3 < 5 + 6 * 7 + 3 * 23 + 8 /  (1 + 2) ;"));
@@ -36,7 +36,7 @@ public class ADLRulesSerializerTest {
     public void matches() throws Exception {
         Archetype archetype = load("matches.adls");
         String serializedADL = ADLArchetypeSerializer.serialize(archetype);
-        //System.out.println(serializedADL);
+
         assertTrue(serializedADL.contains("$extended_validity:Boolean ::= /data[id2]/events[id3]/data[id4]/items[id5]/value/magnitude matches {|0.0..30.0|};"));
     }
 
@@ -44,7 +44,7 @@ public class ADLRulesSerializerTest {
     public void modelReferences() throws Exception {
         Archetype archetype = load("modelreferences.adls");
         String serializedADL = ADLArchetypeSerializer.serialize(archetype);
-        //System.out.println(serializedADL);
+
         assertTrue(serializedADL.contains("$arithmetic_test:Real ::= /data[id2]/events[id3]/data[id4]/items[id5]/value/magnitude"));
         assertTrue(serializedADL.contains("blood_pressure_valid: $arithmetic_test > 50;"));
 
@@ -54,7 +54,7 @@ public class ADLRulesSerializerTest {
     public void forAll() throws Exception {
         Archetype archetype = load("for_all.adls");
         String serializedADL = ADLArchetypeSerializer.serialize(archetype);
-        System.out.println(serializedADL);
+
         assertTrue(serializedADL.contains("blood_pressure_valid: for_all $event in /data[id2]/events\n" +
                 "        satisfies $event/data[id4]/items[id5]/value/magnitude > $event/data[id4]/items[id6]/value/magnitude - 5;"));
     }
