83dfdb4a786a8e9b6f9d9b5fe9f31079a7de279f
Pieter Bos <pieter.bos@nedap.com>
2016-07-22 15:37:26 +0200

    Use kryo 4.0.0, remove now obsolete jsr-310 kryo extensions

--- src/main/java/com/nedap/archie/util/KryoUtil.java
+++ src/main/java/com/nedap/archie/util/KryoUtil.java
@@ -3,7 +3,6 @@ package com.nedap.archie.util;
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.pool.KryoFactory;
 import com.esotericsoftware.kryo.pool.KryoPool;
-import com.guestful.jsr310.kryo.KryoJsr310;
 
 /**
  * Created by pieter.bos on 03/11/15.
@@ -17,7 +16,6 @@ public class KryoUtil {
         KryoFactory factory = new KryoFactory() {
             public Kryo create() {
                 Kryo kryo = new Kryo();
-                KryoJsr310.addJsr310Serializers(kryo); //needed for all java.time types
                 return kryo;
             }
         };
