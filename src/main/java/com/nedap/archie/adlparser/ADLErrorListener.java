6db2187fc4a5147c16204664e61607a320703418
Pieter Bos <pieter.bos@nedap.com>
2017-03-09 23:21:04 +0100

    Set some parser warnings that aren't avoidable with current grammar to debug level

--- src/main/java/com/nedap/archie/adlparser/ADLErrorListener.java
+++ src/main/java/com/nedap/archie/adlparser/ADLErrorListener.java
@@ -36,19 +36,19 @@ public class ADLErrorListener implements ANTLRErrorListener {
     public void reportAmbiguity(Parser recognizer, DFA dfa, int startIndex, int stopIndex, boolean exact, BitSet ambigAlts, ATNConfigSet configs) {
         String input = recognizer.getInputStream().getText(new Interval(startIndex, stopIndex));
         String warning = String.format("FULL AMBIGUITY: %d-%d, exact: %b, input: %s", startIndex, stopIndex, exact, input);
-        logger.warn(warning);
+        logger.debug(warning);
         errors.addWarning(warning);
     }
 
     @Override
     public void reportAttemptingFullContext(Parser recognizer, DFA dfa, int startIndex, int stopIndex, BitSet conflictingAlts, ATNConfigSet configs) {
         String input = recognizer.getInputStream().getText(new Interval(startIndex, stopIndex));
-        logger.warn("FULL CONTEXT: {}-{}, alts: {}, {}", startIndex, stopIndex, conflictingAlts, input);
+        logger.debug("FULL CONTEXT: {}-{}, alts: {}, {}", startIndex, stopIndex, conflictingAlts, input);
     }
 
     @Override
     public void reportContextSensitivity(Parser recognizer, DFA dfa, int startIndex, int stopIndex, int prediction, ATNConfigSet configs) {
-        logger.warn("CONTEXT SENSITIVITY: {}-{}, prediction: {}", startIndex, stopIndex, prediction);
+        logger.debug("CONTEXT SENSITIVITY: {}-{}, prediction: {}", startIndex, stopIndex, prediction);
     }
 
     public ADLParserErrors getErrors() {
