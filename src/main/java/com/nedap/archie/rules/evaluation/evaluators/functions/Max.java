624e5960b1ae32a23d3b7bd0807421dc09a813f3
Pieter Bos <pieter.bos@nedap.com>
2017-04-10 13:52:49 +0200

    Fix type of function results

--- src/main/java/com/nedap/archie/rules/evaluation/evaluators/functions/Max.java
+++ src/main/java/com/nedap/archie/rules/evaluation/evaluators/functions/Max.java
@@ -1,5 +1,6 @@
 package com.nedap.archie.rules.evaluation.evaluators.functions;
 
+import com.nedap.archie.rules.PrimitiveType;
 import com.nedap.archie.rules.evaluation.FunctionCallException;
 import com.nedap.archie.rules.evaluation.FunctionImplementation;
 import com.nedap.archie.rules.evaluation.Value;
@@ -27,6 +28,7 @@ public class Max implements FunctionImplementation {
         //if one of the values is null, return null.
         ValueList possiblyNullResult = checkAndHandleNull(arguments);
         if(possiblyNullResult != null) {
+            possiblyNullResult.setType(PrimitiveType.Real);
             return possiblyNullResult;
         }
         //check that all valueList are equal length or 1 length
@@ -35,7 +37,7 @@ public class Max implements FunctionImplementation {
             throw new FunctionCallException("value lists of min operator not the same length");
         }
         ValueList result = new ValueList();
-
+        result.setType(PrimitiveType.Real);
         for(int i = 0; i < length; i++) {
             Double max = null;
             List<String> paths = new ArrayList<>();
--- src/main/java/com/nedap/archie/rules/evaluation/evaluators/functions/Min.java
+++ src/main/java/com/nedap/archie/rules/evaluation/evaluators/functions/Min.java
@@ -1,5 +1,6 @@
 package com.nedap.archie.rules.evaluation.evaluators.functions;
 
+import com.nedap.archie.rules.PrimitiveType;
 import com.nedap.archie.rules.evaluation.FunctionCallException;
 import com.nedap.archie.rules.evaluation.FunctionImplementation;
 import com.nedap.archie.rules.evaluation.Value;
@@ -25,6 +26,7 @@ public class Min implements FunctionImplementation {
         //if one of the values is null, return null.
         ValueList possiblyNullResult = checkAndHandleNull(arguments);
         if(possiblyNullResult != null) {
+            possiblyNullResult.setType(PrimitiveType.Real);
             return possiblyNullResult;
         }
         //check that all valueList are equal length or 1 length
@@ -33,7 +35,7 @@ public class Min implements FunctionImplementation {
             throw new FunctionCallException("value lists of min operator not the same length");
         }
         ValueList result = new ValueList();
-
+        result.setType(PrimitiveType.Real);
         for(int i = 0; i < length; i++) {
             Double min = null;
             List<String> paths = new ArrayList<>();
